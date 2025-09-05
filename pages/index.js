59a3c3637685f296d09f5d8b1d4d27417ee6c860
Guilherme Salvador <101298447+guisalva@users.noreply.github.com>
2025-09-05 16:48:22 +0000

    modificado página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+const fraseDoDia = "Oi minha lindeza, esse é um pequeno espaço na internet onde pensei em dedicar à você enquanto estudo, te amo muitoooo 😊."
+
 function Home() {
-    return <>
-        <h1>Gabs, eu amo muito você ❤</h1>
-        <p>Olhe aqui e dê um sorrisinho 😊</p>
-    </>
+    return <main style={{ position: "absolute", top: "40%", left: "50%", translate: "-50% -50%", textAlign: "center", color: "#333333", fontFamily: "sans-serif" }}>
+        <h1>Recadinho do dia ❤️</h1>
+        <div style={{ width: "400px", border: "1px solid #ccccccff", borderRadius: "0.5rem", paddingInline: "1rem", fontStyle: "italic" }}>
+            <p style={{ color: "#525252ff"  }}>"{fraseDoDia}"</p>
+            <p style={{ textAlign: "end" }}>- seu Mo</p>
+        </div>
+    </main>
 }
 
 export default Home;
