570f511e72e3f95204cf5c44f143b15156798231
Guilherme Salvador <101298447+guisalva@users.noreply.github.com>
2025-09-10 19:25:30 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
-const fraseDoDia = "Oi minha lindeza, esse é um pequeno espaço na internet onde pensei em dedicar à você enquanto estudo, te amo muitoooo 😊."
+const fraseDoDia =
+  "Oi minha lindeza, esse é um pequeno espaço na internet onde pensei em dedicar à você enquanto estudo, te amo muitoooo 😊.";
 
 function Home() {
-    return <main style={{ position: "absolute", top: "40%", left: "50%", translate: "-50% -50%", textAlign: "center", color: "#333333", fontFamily: "sans-serif" }}>
-        <h1>Recadinho do dia ❤️</h1>
-        <div style={{ width: "400px", border: "1px solid #ccccccff", borderRadius: "0.5rem", paddingInline: "1rem", fontStyle: "italic" }}>
-            <p style={{ color: "#525252ff"  }}>"{fraseDoDia}"</p>
-            <p style={{ textAlign: "end" }}>- seu Mo</p>
-        </div>
+  return (
+    <main
+      style={{
+        position: "absolute",
+        top: "40%",
+        left: "50%",
+        translate: "-50% -50%",
+        textAlign: "center",
+        color: "#333333",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Recadinho do dia ❤️</h1>
+      <div
+        style={{
+          width: "400px",
+          border: "1px solid #ccccccff",
+          borderRadius: "0.5rem",
+          paddingInline: "1rem",
+          fontStyle: "italic",
+        }}
+      >
+        <p style={{ color: "#525252ff" }}>"{fraseDoDia}"</p>
+        <p style={{ textAlign: "end" }}>- seu Mo</p>
+      </div>
     </main>
+  );
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
